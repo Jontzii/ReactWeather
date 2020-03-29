5969e868c91a265faa8aabe5b5fcd2c9ce4924d1
Joonas Hiltunen <joonas.hiltunen@tuni.fi>
2020-03-29 02:35:18 +0200

    Updates

---
+++ precache-manifest.964d72b6d13713dd264c27f1b36c4faf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35d14be0e03a95565eb2054f33505dde",
+    "url": "/ReactWeather/index.html"
+  },
+  {
+    "revision": "f8a98090ebb2668d9e55",
+    "url": "/ReactWeather/static/css/main.4fa14513.chunk.css"
+  },
+  {
+    "revision": "4f5e8d6e1126f8c7782d",
+    "url": "/ReactWeather/static/js/2.1e8c7895.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/ReactWeather/static/js/2.1e8c7895.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8a98090ebb2668d9e55",
+    "url": "/ReactWeather/static/js/main.81f3bc78.chunk.js"
+  },
+  {
+    "revision": "c0d18493c71b492812e7",
+    "url": "/ReactWeather/static/js/runtime-main.5171e3fd.js"
+  }
+]);
