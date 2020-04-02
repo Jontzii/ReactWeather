db71f7cfca127c9dc74edb54a5a8a60795ba05ac
Joonas Hiltunen <joonas.hiltunen@tuni.fi>
2020-04-02 03:22:37 +0300

    Updates

---
+++ precache-manifest.f22f50a73c2d4ef0281b8d65797cd52d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64ef7493c263e9693f8b1ba7db4ad299",
+    "url": "/ReactWeather/index.html"
+  },
+  {
+    "revision": "6fc72412c401b3935de5",
+    "url": "/ReactWeather/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "a53f175e9810e653d77d",
+    "url": "/ReactWeather/static/css/main.c14c564b.chunk.css"
+  },
+  {
+    "revision": "6fc72412c401b3935de5",
+    "url": "/ReactWeather/static/js/2.59f7bb66.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/ReactWeather/static/js/2.59f7bb66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a53f175e9810e653d77d",
+    "url": "/ReactWeather/static/js/main.3ba9dce0.chunk.js"
+  },
+  {
+    "revision": "c0d18493c71b492812e7",
+    "url": "/ReactWeather/static/js/runtime-main.5171e3fd.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.47489547f8dcb773f732b80c3d7f216e.js"
+  "/ReactWeather/precache-manifest.f22f50a73c2d4ef0281b8d65797cd52d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ReactWeather/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
